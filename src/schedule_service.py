1b81f28196340583dc8fc718799ca0b9bc39303f
Oleh Onufryk <olehonufryk@gmail.com>
2024-08-26 21:50:20 +0300

    Fixed requests blocked by CloudFlare (#1)

--- src/schedule_service.py
+++ src/schedule_service.py
@@ -31,9 +31,10 @@ def get_schedule_for_tomorrow():
 
 
 def get_image_urls():
-    result = requests.get(API_LVIV_URL_SCHEDULE_IMAGE).content
+    headers = {'accept': 'application/json', 'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36'}
+    result = requests.get(API_LVIV_URL_SCHEDULE_IMAGE, headers=headers).content
     data = json.loads(result)
-    children = currentSchedule = data['hydra:member'][0]['menuItems'][1]['children']
+    children = currentSchedule = data[0]['menuItems'][1]['children']
     scheduleTomorow =  None
     currentSchedule = None
     for chield in children[::-1]:
